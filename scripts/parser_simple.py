b84746a35d0a34182b8eb29958118cce2d14e9fb
michalmonday <michalmonday17@gmail.com>
2021-12-15 20:01:02 +0000

    *

--- scripts/parser_simple.py
+++ scripts/parser_simple.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
 import re
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import argparse
+import sys
 
 from extract_function_ranges_from_llvm_objdump import extract_function_ranges_from_llvm_objdump
 
@@ -24,7 +26,8 @@ def get_pc(fname, main_addr, max_pc=None):
             val = int( val.group(1) , 16)
             if max_pc and max_pc < val: continue
             pc.append(val)
-    return pd.DataFrame(pc, columns=[fname])
+    # return pd.DataFrame(pc, columns=[fname])
+    return pc
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -35,7 +38,7 @@ if __name__ == '__main__':
             )
 
     parser.add_argument(
-            '-max'
+            '-max',
             '--max-pc',
             type=int,
             required=False,
@@ -45,23 +48,45 @@ if __name__ == '__main__':
 
     parser_group = parser.add_mutually_exclusive_group(required=True)
     parser_group.add_argument(
-            '-L',
-            '--llvm-objdump',
+            '-fr',
+            '--function-ranges',
             type=argparse.FileType('r'),
-            required=False,
-            help='File name containing output of llvm-objdump.'
+            help='File name containing output of extract_function_ranges_from_llvm_objdump.py'
             )
 
-    parser_group.add_argument(
-            '-m'
-            '--main-addr',
-            type=int,
-            required=False,
-            default=None,
-            help='Entry address of main function.'
-            )
+    #parser_group.add_argument(
+    #        '-L',
+    #        '--llvm-objdump',
+    #        type=argparse.FileType('r'),
+    #        required=False,
+    #        help='File name containing output of llvm-objdump.'
+    #        )
+
+
+    #parser_group.add_argument(
+    #        '-m',
+    #        '--main-addr',
+    #        type=int,
+    #        required=False,
+    #        default=None,
+    #        help='Entry address of main function.'
+    #        )
 
     args = parser.parse_args()
-    df = get_pc(args.normal_log.name, main_addr, max_pc = args.max_pc)
 
-    extract_function_ranges_from_llvm_objdump()
+    #if args.main_addr:
+    #    main_addr = args.main_addr
+    #elif args.llvm_objdump:
+    #    ranges = extract_function_ranges_from_llvm_objdump(args.llvm_objdump.name)
+    #    main_addr = ranges['main'][0]
+    #elif args.function_ranges:
+    #    main_addr = json.load(args.function_ranges)['main'][0]
+
+    program_counters = get_pc(args.logfile.name, main_addr, max_pc = args.max_pc)
+    
+    print(program_counters)
+    # for pc in program_counters:
+    #     print(pc)
+    
+    
+
