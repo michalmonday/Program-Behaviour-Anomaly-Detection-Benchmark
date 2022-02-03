a47a99718d986f70af7e06a34bf3d239548864f6
michalmonday <michalmonday17@gmail.com>
2022-02-03 13:17:32 +0000

    LSTM autoencoder progress

--- scripts/lstm_autoencoder/time-series-anomaly-detection.py
+++ scripts/lstm_autoencoder/time-series-anomaly-detection.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-get_ipython().system('nvidia-smi')
-
-
-# In[ ]:
-
-
-get_ipython().system('pip install gdown')
-get_ipython().system('pip install tensorflow-gpu')
-
-
-# In[2]:
-
-
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -26,9 +10,6 @@ from pylab import rcParams
 import matplotlib.pyplot as plt
 from matplotlib import rc
 from pandas.plotting import register_matplotlib_converters
-
-get_ipython().run_line_magic('matplotlib', 'inline')
-get_ipython().run_line_magic('config', "InlineBackend.figure_format='retina'")
 
 register_matplotlib_converters()
 sns.set(style='whitegrid', palette='muted', font_scale=1.5)
@@ -40,49 +21,20 @@ RANDOM_SEED = 42
 np.random.seed(RANDOM_SEED)
 tf.random.set_seed(RANDOM_SEED)
 
-
-# In[3]:
-
-
 print(sns)
-
-
-# In[4]:
-
-
-get_ipython().system('gdown --id 10vdMg_RazoIatwrT7azKFX4P02OebU76 --output spx.csv')
-
-
-# In[5]:
-
 
 df = pd.read_csv('spx.csv', parse_dates=['date'], index_col='date')
 
-
-# In[6]:
-
-
 df.head()
 
-
-# In[7]:
-
-
 plt.plot(df, label='close price')
-plt.legend();
-
-
-# In[8]:
-
+plt.legend()
+plt.show()
 
 train_size = int(len(df) * 0.95)
 test_size = len(df) - train_size
 train, test = df.iloc[0:train_size], df.iloc[train_size:len(df)]
 print(train.shape, test.shape)
-
-
-# In[9]:
-
 
 from sklearn.preprocessing import StandardScaler
 
@@ -92,10 +44,6 @@ scaler = scaler.fit(train[['close']])
 train['close'] = scaler.transform(train[['close']])
 test['close'] = scaler.transform(test[['close']])
 
-
-# In[10]:
-
-
 def create_dataset(X, y, time_steps=1):
     Xs, ys = [], []
     for i in range(len(X) - time_steps):
@@ -103,10 +51,6 @@ def create_dataset(X, y, time_steps=1):
         Xs.append(v)        
         ys.append(y.iloc[i + time_steps])
     return np.array(Xs), np.array(ys)
-
-
-# In[11]:
-
 
 TIME_STEPS = 30
 
@@ -116,9 +60,6 @@ X_train, y_train = create_dataset(train[['close']], train.close, TIME_STEPS)
 X_test, y_test = create_dataset(test[['close']], test.close, TIME_STEPS)
 
 print(X_train.shape)
-
-
-# In[12]:
 
 
 model = keras.Sequential()
@@ -133,93 +74,67 @@ model.add(keras.layers.Dropout(rate=0.2))
 model.add(keras.layers.TimeDistributed(keras.layers.Dense(units=X_train.shape[2])))
 model.compile(loss='mae', optimizer='adam')
 
-
-# In[ ]:
-
-
 history = model.fit(
     X_train, y_train,
     epochs=10,
-    batch_size=32,
+    batch_size=5000,#32,
     validation_split=0.1,
     shuffle=False
 )
 
-
-# In[ ]:
-
-
 plt.plot(history.history['loss'], label='train')
 plt.plot(history.history['val_loss'], label='test')
 plt.legend();
-
-
-# In[ ]:
-
+plt.show()
 
 X_train_pred = model.predict(X_train)
 
 train_mae_loss = np.mean(np.abs(X_train_pred - X_train), axis=1)
 
-
-# In[ ]:
-
-
 sns.distplot(train_mae_loss, bins=50, kde=True);
-
-
-# In[ ]:
-
 
 X_test_pred = model.predict(X_test)
 
 test_mae_loss = np.mean(np.abs(X_test_pred - X_test), axis=1)
 
+# THRESHOLD = 0.65
+THRESHOLD = 1.7
 
-# In[ ]:
+# results_df = pd.DataFrame(index=test[TIME_STEPS:].index) #(index=test[TIME_STEPS:].index)
 
+results_df = pd.DataFrame(index=df.index.values[TIME_STEPS:-TIME_STEPS])
+results_df['loss'] = np.concatenate((train_mae_loss, test_mae_loss))
+results_df['threshold'] = THRESHOLD
+results_df['anomaly'] = results_df.loss > results_df.threshold
+# results_df['close'] = test[TIME_STEPS:].close
+# results_df[['train_loss', 'test_loss', 'threshold', 'anomaly']].plot()
+ax = results_df[['loss']].plot()
+ax.axvline(results_df.index.values[train_size], color='k', linestyle='--')
+# plt.plot(train_mae_loss, label='train_loss')
+plt.show()
 
-THRESHOLD = 0.65
+#plt.plot(results_df.index, results_df.loss, label='loss')
+#plt.plot(results_df.index, results_df.threshold, label='threshold')
+#plt.xticks(rotation=25)
+#plt.legend()
+#plt.show()
 
-test_score_df = pd.DataFrame(index=test[TIME_STEPS:].index)
-test_score_df['loss'] = test_mae_loss
-test_score_df['threshold'] = THRESHOLD
-test_score_df['anomaly'] = test_score_df.loss > test_score_df.threshold
-test_score_df['close'] = test[TIME_STEPS:].close
-
-
-# In[ ]:
-
-
-plt.plot(test_score_df.index, test_score_df.loss, label='loss')
-plt.plot(test_score_df.index, test_score_df.threshold, label='threshold')
-plt.xticks(rotation=25)
-plt.legend();
-
-
-# In[ ]:
-
-
-anomalies = test_score_df[test_score_df.anomaly == True]
+anomalies = results_df[results_df.anomaly == True]
 anomalies.head()
-
-
-# In[ ]:
-
 
 plt.plot(
   test[TIME_STEPS:].index, 
-  scaler.inverse_transform(test[TIME_STEPS:].close), 
+  scaler.inverse_transform(test[TIME_STEPS:].close.values.reshape(1,-1)).reshape(-1), 
   label='close price'
 );
 
 sns.scatterplot(
   anomalies.index,
-  scaler.inverse_transform(anomalies.close),
+  scaler.inverse_transform(anomalies.close.values.reshape(1,-1)).reshape(-1),
   color=sns.color_palette()[3],
   s=52,
   label='anomaly'
 )
 plt.xticks(rotation=25)
 plt.legend();
-
+plt.show()
